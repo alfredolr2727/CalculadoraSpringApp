441bdc9472c678976dc58c63b4777f96036e5771
alfredo.lopez <alfredo.lopez@altia.es>
2022-02-21 17:19:05 +0100

    Proyecto generado con Spring Initializer, con dependencias

---
+++ src/test/java/com/calculadora/CalculadoraApp/CalculadoraAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calculadora.CalculadoraApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadoraAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
